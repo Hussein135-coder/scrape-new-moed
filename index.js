981db40f2981205fd7e569ab7ec348db07abf944
Hussein135-coder <80627987+Hussein135-coder@users.noreply.github.com>
2023-07-24 09:25:46 +0300

    Add asasy

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const { JSDOM } = require('jsdom');
 const baseUrl = process.env.URL || `http://moed.gov.sy/12th/index.php`;
 const baseUrl2 = process.env.URL2 ||`http://moed.gov.sy/12th/resultpage.php`;
 
-
+const asasyUrl = process.env.URL || `http://moed.gov.sy/9th/index.php`;
+const asasyUrl2 = process.env.URL2 ||`http://moed.gov.sy/9th/result.php`;
 
 const app = express()
 
@@ -67,6 +68,48 @@ try{
 }
 })
 
+let tryCountAsasy = 5;
+app.post('/asasy', async (req, res) => {
+	tryCountAsasy = 5;
+	console.log(tryCountAsasy)
+	const num = req.body.id;
+	const city = req.body.city;
+   	 const branch =  req.body.br;
+
+console.log(num,city,branch)
+try{
+	let data = await resultAsasy(num,city,branch)
+	while(tryCountAsasy > 0){	
+		if(data == 'error'){
+			data = await resultAsasy(num,city,branch)
+		}else if(data == null){
+			res.json({status : "error" , message : null});
+			tryCountAsasy = 0
+			return
+		}else if(data.length == 0){
+			res.json({status : "notFound" , message : data})
+			tryCountAsasy = 0
+			return
+		}else{
+			res.json({status : "success" , message : data})
+			tryCountAsasy = 0
+			return
+		}
+	}
+		if(data == 'error' || data == null){
+			res.json({status : "error" , message : null});
+		}else if(data.length == 0){
+			res.json({status : "notFound" , message : data})
+		}else{
+			res.json({status : "success" , message : data})	
+		}
+	
+	
+
+}catch(err){
+	res.json({status : "error" , message : null});
+}
+})
 
 let cookies = ''
 
@@ -134,7 +177,7 @@ const resultNum = async (num,city,branch,sub)=>{
 			message['name']  = name?.textContent
 			message['name4']  = mother?.textContent
 			message['school']  = school?.textContent
-			message['result']  = result?.textContent.trim() == "ناجح" ? 1 : 3
+			message['result']  = result?.textContent.trim() == "ناجح" ? 1 : result?.textContent.trim() == "راسب" ? 3 : 0 
 
 			
 			for (let i = 0; i < subjects.length; i++) {
@@ -146,6 +189,110 @@ const resultNum = async (num,city,branch,sub)=>{
 			
 			if(errorReq != null){
 				console.log(tryCount)
+				return 'error';
+			}else if(notFound != null){
+				return [];
+			}else if(message['name']){
+				return [message];
+			}else{
+				return null
+			}
+			
+}
+	catch (error) {
+		console.log(error)
+		return null
+	}
+}
+
+
+
+let cookiesAsasy = ''
+
+const optionsAsasy = {
+	method :'get',
+	url : asasyUrl,  
+	headers: {
+		"Host" : "moed.gov.sy",
+		"User-Agent" : "Mozilla/5.0"
+	}
+}
+
+
+// Number
+const resultAsasy = async (num,city,branch)=>{
+    try {
+	   		tryCount--;		
+	    	const formData = new FormData();
+			formData.append("stdnum",num);
+			formData.append("branch", branch);
+			formData.append("city", city);
+
+
+			const response = await axios.request(optionsAsasy)
+	        cookiesAsasy =await response.headers.get('set-cookie')
+			const data = await response.data;
+			
+			console.log(cookiesAsasy)
+			const optionsAsasy2 = {
+				method: "post",
+				url :asasyUrl2,
+				headers: {
+					"Host" : "moed.gov.sy",
+					"Referer" : asasyUrl,
+					"Origin" : "http://moed.gov.sy",
+				   "User-Agent" : "Mozilla/5.0 ",
+					 "Upgrade-Insecure-Requests" : "1",
+				 "Content-Type" : "application/x-www-form-urlencoded",
+				 "Cookie" : cookiesAsasy[0]
+				},
+				data : formData
+			}
+
+			let message = {}
+			const response2 = await axios.request(optionsAsasy2)
+			const html = await response2.data;
+
+			const dom = new JSDOM(html);
+			const document = dom.window.document;
+
+			const subjects = document.querySelectorAll('.subject-con .per-subject .subject-title');
+			const marks = document.querySelectorAll('.subject-mark span:last-child');
+			const marksUp = document.querySelectorAll('.max span:last-child');
+			const marksDown = document.querySelectorAll('.min span:last-child ');
+			const id = document.querySelector('.student-info .info-1 div:last-child');
+			const name = document.querySelector('.student-info .info-4 div:last-child');
+			const mother = document.querySelector('.student-info .info-5 div:last-child');
+			const school = document.querySelector('.student-info .info-6 div:last-child');
+			const result = document.querySelector('.student-info .info-7 div:last-child');
+			const nccd = document.querySelector('.nccd-msg a');
+			const olympic = document.querySelector('.olympic-msg a');
+	    	const errorReq = document.querySelector('.message');
+			const notFound = document.querySelector('.be-sure');
+
+			message['id']  = id?.textContent
+			message['name']  = name?.textContent
+			message['name4']  = mother?.textContent
+			message['school']  = school?.textContent
+			message['result']  = result?.textContent.trim() == "ناجح" ? 1 : result?.textContent.trim() == "راسب" ? 3 : 0 
+
+			message['nccd']  = nccd ? 1 : 0
+			message['nccdTitle']  = nccd?.textContent
+			message['nccdUrl']  = nccd?.href
+			
+			message['olympic']  = olympic ? 1 : 0
+			message['olympicTitle']  = olympic?.textContent
+			message['olympicUrl']  = olympic?.href
+
+			for (let i = 0; i < subjects.length; i++) {
+				message['m'+(i+1)+'n'] = subjects[i].textContent.trim()
+				message['m'+(i+1)+'a'] = marksDown[i].textContent.trim()
+				message['m'+(i+1)+'b'] = marksUp[i].textContent.trim()
+				message['m'+(i+1)] = marks[i].textContent.trim()
+			}
+			
+			if(errorReq != null){
+				console.log(tryCountAsasy)
 				return 'error';
 			}else if(notFound != null){
 				return [];
